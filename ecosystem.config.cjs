3db046aa026bfd1157939bd77ab4459750d8771f
Mykola Zghurskyi <mykola@techno4.online>
2026-09-11 04:35:03 +0300

    2.0.0 feat: add PM2 ecosystem config and npm scripts for T4F2 server

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+﻿module.exports = {
+  apps: [
+    {
+      name: 'T4F2',
+      script: '../../node_modules/vite/bin/vite.js',
+      cwd: 'apps/techno4-framework2-boonker',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+        TECHNO4_THREADS_PORT: '8008'
+      }
+    }
+  ]
+};
